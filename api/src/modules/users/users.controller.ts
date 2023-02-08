0355d589a5dba1b5715056d94c8085c8ab7ce50a
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-08 13:56:10 +0100

    Delete unwated functions after change it to auth module

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,30 +1,24 @@
 import { Body, Controller, Get, Post,UsePipes,ValidationPipe } from '@nestjs/common';
 import { Param, Query } from '@nestjs/common/decorators';
-import { CreateUserDto } from './dto/createUser.dto';
-import { SignUserDto } from './dto/SignUser.dto';
+import { CreateUserDto } from '../Auth/dto/RegiterUser.dto';
+import { SignUserDto } from '../Auth/dto/SignUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private authService : UsersService){}
+    constructor(private UsersService : UsersService){}
     // @Get('')
     // getUsers(@Query('filter') filter: String) {
     //     console.log(filter);
 
     //     return { med: "tester" }
     // }
-    @Post('AddUser')
-    @UsePipes(new ValidationPipe())
-    AddNewUser(@Body() userData: CreateUserDto):Promise<{token:String}> {
-        return this.authService.SignUp(userData)
-    }
-    @Post('SignUser')
-    @UsePipes(new ValidationPipe())
-    SignInUser(@Body() signInData:SignUserDto):Promise<{token:String}>{
-        return this.authService.SignIn(signInData)
+    @Get('AllUsers')
+    FetchUsers(){
+        return this.UsersService.FetchUsers()
     }
     @Get(':id')
     getUserById(@Param('id') id: String) {
-        return { id }
+        return this.UsersService.GetUserById(id)
     }
 }
--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException,HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schema/user.schema';
-import { CreateUserDto } from './dto/createUser.dto';
+import { CreateUserDto } from '../Auth/dto/RegiterUser.dto';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt/dist';
-import { SignUserDto } from './dto/SignUser.dto';
+import { SignUserDto } from '../Auth/dto/SignUser.dto';
 @Injectable()
 export class UsersService {
     constructor(
@@ -13,36 +13,20 @@ export class UsersService {
         private userModel:Model<User>,
         private jwtService:JwtService
     ){}
-
-    async SignUp(CreateUserDto:CreateUserDto):Promise<{token:string}> {
-        const {FirstName,LastName,Email,Phone, Birthday,Password}=CreateUserDto
-        const hashedPassword = await bcrypt.hash(Password,10) 
-
-
-        const user = await this.userModel.create({
-            FirstName,
-            LastName,
-            Email,
-            Phone, 
-            Birthday,
-            Password:hashedPassword
-        })
-
-        const token = this.jwtService.sign({id:user._id})
-        return {token}
-    }
-    async SignIn(SignUserDto:SignUserDto):Promise<{token:String}>{
-        const {Email ,Password} = SignUserDto
-        const user = await this.userModel.findOne({Email})
-        if (!user) {
-            throw new UnauthorizedException('invalide email or password')
+    async GetUserById(id:String):Promise<{user}>{
+        const user = await this.userModel.findById(id).select('-Password')
+        if (!user) {  
+            throw new NotFoundException(`User Not found`);
+        } else {
+            return {user}
         }
-        const isPasswordValide = await bcrypt.compare(Password,user.Password)
-        if (!isPasswordValide) {
-            throw new UnauthorizedException('invalide email or password')
-        }
-        const token = this.jwtService.sign({id:user._id})
-        return {token}
     }
-
+    async FetchUsers():Promise<{users}>{
+        const users = await this.userModel.find().select('-Password')
+        if (!users) {
+            throw new  HttpException('No Users found',HttpStatus.BAD_REQUEST)
+        }else{
+            return {users}
+        }
+    }
 }
