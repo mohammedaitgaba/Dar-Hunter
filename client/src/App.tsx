22c65933be9cc477dd271cbbd9e4dcc2a7cccc21
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-24 14:47:27 +0100

    initilize frontend folders using react vite + ts

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="w-full">
+          <h1 className="underline self-center">
+            Hello world!
+          </h1>
+    </div>
+  )
+}
+
+export default App
