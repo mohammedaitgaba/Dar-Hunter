0ebd006333123200a48dc17d2b23491b856ba5fd
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-25 14:35:47 +0100

    add tailwind to create special and more complex ui

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,9 +2,18 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],  
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary and secondary are the dominant colors
+        primary: "#000080", // navy blue (dark)
+        secondary: "#ADD8E6", // light blue (dark)
+        black: "#050401", // blue (dark)
+        gray: "#6E7271", // grey (dark)
+        lightGray: "#DEE5E5",
+      },
+    },
   },
   plugins: [],
-}
+};
