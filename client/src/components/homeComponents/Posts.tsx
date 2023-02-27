3fad985a7a741ce2f0797cd3c62aab0a0c8b683f
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-27 19:21:31 +0100

    fix posts responsive , add like icon and pagination ui

--- client/src/components/homeComponents/Posts.tsx
+++ client/src/components/homeComponents/Posts.tsx
@@ -6,13 +6,20 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions, IconButton } from '@mui/material';
-import ShareIcon from '@mui/icons-material/Share';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import { CardActionArea} from '@mui/material';
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import image from '../../assets/ekko.jpg'
 
 const Posts = () => {
   const [postsStyleSwitcher,setPostsStyleSwitcher] = React.useState(false)
+  const [like ,setLike]= React.useState(false)
+  let items=[]
+  for (let i = 0; i < 12; i++) {
+    items.push(i)
+  }
   return (
     <div className='flex flex-col'>
         <div className="shadow-lg p-4 w-full self-center bg-white" >
@@ -20,88 +27,44 @@ const Posts = () => {
           <ViewAgendaOutlinedIcon fontSize="large" sx={postsStyleSwitcher?{color:'#000080'}:{color:'#ADD8E6'}} onClick={()=>setPostsStyleSwitcher(true)}/>
         </div>
         <div className='flex flex-wrap py-6 md:justify-between justify-center'>
-          <Card sx={!postsStyleSwitcher?{maxWidth: 345}:{width:'100%'}}>
-            <CardActionArea sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex',marginBottom:5}}>
-              <CardMedia
-                component="img"
-                image={image}
-                alt="Profile pic" 
-                sx={!postsStyleSwitcher?{height:'320px',maxHeight:'320px'}:{maxWidth:'200px'}}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-              </CardActions>
-            </CardActionArea>
-          </Card>
-          <Card sx={!postsStyleSwitcher?{maxWidth: 345}:{width:'100%'}}>
-            <CardActionArea sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex',marginBottom:5}}>
-              <CardMedia
-                component="img"
-                image={image}
-                alt="Profile pic" 
-                sx={!postsStyleSwitcher?{height:'320px',maxHeight:'320px'}:{maxWidth:'200px'}}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-              </CardActions>
-            </CardActionArea>
-          </Card>
-          <Card sx={!postsStyleSwitcher?{maxWidth: 345}:{width:'100%'}}>
-            <CardActionArea sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex',marginBottom:5}}>
-              <CardMedia
-                component="img"
-                image={image}
-                alt="Profile pic" 
-                sx={!postsStyleSwitcher?{height:'320px',maxHeight:'320px'}:{maxWidth:'200px'}}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-              </CardActions>
-            </CardActionArea>
-          </Card>
+          {
+            items.map((item)=>(
+            <Card sx={!postsStyleSwitcher?{maxWidth: 345,position:'relative',marginBottom:5}:{width:'100%',position:'relative',marginBottom:5}}>
+              <CardActionArea  sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex'}} >
+                <CardMedia
+                  component="img"
+                  image={image}
+                  alt="Profile pic" 
+                  sx={!postsStyleSwitcher?{height:'280px',maxHeight:'300px'}:{maxWidth:'200px'}}
+                />
+                <FavoriteRoundedIcon color={'action'} sx={{position:'absolute', top:'5px',right:'5px',backgroundColor:'white',borderRadius:'50%',width:'35px',height:'35px'}}/>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+  
+                <CardContent sx={{ width:'100%',display:'flex', justifyContent:'space-between'}}>
+                  <Typography gutterBottom variant="h6" component="div" sx={{fontWeight:'600'}}>
+                    1000dh
+                  </Typography>
+                  <BookmarkBorderOutlinedIcon fontSize="medium"/>
+                </CardContent>
+  
+              </CardActionArea>
+            </Card>
+            ))
+          }
 
+        </div>
+        <div className='self-center pt-4 '>
+          <Stack spacing={2}>
+              <Pagination count={10} shape="rounded" color="primary"/>
+            </Stack>
         </div>
     </div>
   )
