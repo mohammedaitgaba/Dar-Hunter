c91829173da0923407e0d5453389228ed8ba7442
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-27 19:22:19 +0100

    make premium posts sticky and remove description so it can have 2 premium posts

--- client/src/components/homeComponents/PremiumPosts.tsx
+++ client/src/components/homeComponents/PremiumPosts.tsx
@@ -9,29 +9,47 @@ import image from '../../assets/bannerBackground.png'
 import WorkspacePremiumRoundedIcon from '@mui/icons-material/WorkspacePremiumRounded';
 const PremiumPosts = () => {
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center sticky top-[90px]'>
       <div className="shadow-lg p-4 w-full text-center bg-white">
         <WorkspacePremiumRoundedIcon fontSize="large" sx={{color:'#FFD700'}}/>
         premium
       </div>
       <div className='py-6'>
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 340, width:340,marginBottom:5 }}>
           <CardActionArea>
             <CardMedia
               component="img"
-              height="140"
               image={image}
               alt="Profile pic"
+              sx={{maxHeight:'150px'}}
             />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+            <CardContent sx={{display:'flex',justifyContent:'space-between'}}>
+              <Typography  variant="h6" component="div">
                 Lizard
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
+              <Typography  variant="h6" component="div">
+                1000 Dh
               </Typography>
             </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 340 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              image={image}
+              alt="Profile pic"
+              sx={{maxHeight:'150px'}}
+            />
+            <CardContent sx={{display:'flex',justifyContent:'space-between'}}>
+              <Typography  variant="h6" component="div">
+                Lizard
+              </Typography>
+              <Typography  variant="h6" component="div">
+                1000 Dh
+              </Typography>
+            </CardContent>
+
           </CardActionArea>
         </Card>
       </div>
