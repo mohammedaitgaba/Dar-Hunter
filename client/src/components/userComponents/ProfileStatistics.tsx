131f6a2e983c8b56ee55a005f120495485709b9c
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-03-08 19:06:21 +0100

    add statistics

--- client/src/components/userComponents/ProfileStatistics.tsx
+++ client/src/components/userComponents/ProfileStatistics.tsx
@@ -2,15 +2,57 @@ import React from 'react'
 
 const ProfileStatistics = () => {
   return (
-    <div className='bg-slate-400 flex flex-col justify-between '>
-        <div>
-            med
+    <div className='flex flex-col justify-between lg:w-8/12 lg:ml-4 mt-4 lg:mt-0'>
+        <div className='w-full rounded-xl p-4 shadow-md'>
+            <p>About Properties</p>
+            <div className='flex flex-wrap justify-around'>
+              <div className='flex flex-col items-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='lg:text-lg'>Total Properties</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='lg:text-lg'>Properties Sold</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='lg:text-lg'>Properties on Sell</p>
+              </div>
+            </div>
         </div>
-        <div>
-            hass
+        <div className='w-full shadow-md rounded-xl p-4 mt-4'>
+            <p>Time</p>
+            <div className='flex justify-around'>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+            </div>
         </div>
-        <div>
-            hass
+        <div className='w-full shadow-md rounded-xl p-4 mt-4'>
+            <p>Time</p>
+            <div className='flex justify-around'>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+              <div className='text-center'>
+                  <div className='flex border w-20 h-20 rounded-[50%] items-center justify-center text-4xl text-slate-500'>4</div>
+                  <p className='text-xl'>hasss</p>
+              </div>
+            </div>
         </div>
     </div>
   )
