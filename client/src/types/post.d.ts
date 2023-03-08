2fd171ff502618bdf0f8d7b6f6eeaffbc1ad68a1
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-03-08 20:33:42 +0100

    add type post

---
+++ client/src/types/post.d.ts
@@ -0,0 +1,37 @@
+export interface Post {
+    _id: string
+            Maker: {
+                _id: string,
+                FirstName: string,
+                LastName: string,
+                Email: string,
+                Phone: string,
+                Trusted: boolean,
+                Deleted: boolean,
+                __v: number
+            },
+            Title: string,
+            Description: string,
+            Price: number,
+            PropertyType: string,
+            TransactionType: string,
+            Availability: boolean,
+            Deleted: boolean,
+            City: string,
+            Sector: string,
+            Location: [
+                {
+                    lang: number,
+                    latit: number
+                }
+            ],
+            Details: [
+                {
+                    Rooms: number,
+                    Surface:number ,
+                    floors: number
+                }
+            ],
+            createdAt: Date,
+            updatedAt: Date,
+}
