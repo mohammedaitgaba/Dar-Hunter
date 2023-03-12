c16cafd7d0e0848ac97d62c8fbf9df44cb863983
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-03-12 17:05:09 +0100

    link posts page with single post page and add details section

--- client/src/components/homeComponents/Posts.tsx
+++ client/src/components/homeComponents/Posts.tsx
@@ -15,6 +15,7 @@ import image from '../../assets/ekko.jpg'
 
 import axios from 'axios';
 import { Post } from '../../types/post';
+import { Link } from 'react-router-dom';
 const Posts = () => {
   const [postsStyleSwitcher,setPostsStyleSwitcher] = useState(false)
   const [like ,setLike]= useState(false)
@@ -61,33 +62,35 @@ const Posts = () => {
         <div className='flex flex-wrap py-6 md:justify-between justify-center'>
           {
             posts.map((post:Post)=>(
-            <Card sx={!postsStyleSwitcher?{maxWidth: 345,minWidth:340,position:'relative',marginBottom:5}:{width:'100%',position:'relative',marginBottom:5}}>
-              <CardActionArea  sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex'}} >
-                <CardMedia
-                  component="img"
-                  image={image}
-                  alt="Profile pic" 
-                  sx={!postsStyleSwitcher?{height:'280px',maxHeight:'300px'}:{maxWidth:'200px'}}
-                />
-                <FavoriteRoundedIcon color={'action'} sx={{position:'absolute', top:'5px',right:'5px',backgroundColor:'white',borderRadius:'50%',width:'35px',height:'35px'}}/>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {post.Title}                  
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {post.Description}
-                  </Typography>
-                </CardContent>
-  
-                <CardContent sx={{ width:'100%',display:'flex', justifyContent:'space-between'}}>
-                  <Typography gutterBottom variant="h6" component="div" sx={{fontWeight:'600'}}>
-                    {post.Price}
-                  </Typography>
-                  <BookmarkBorderOutlinedIcon fontSize="medium"/>
-                </CardContent>
-  
-              </CardActionArea>
-            </Card>
+              <Link to={`posts/${post._id}`}>
+                <Card sx={!postsStyleSwitcher?{maxWidth: 345,minWidth:340,position:'relative',marginBottom:5}:{width:'100%',position:'relative',marginBottom:5}}>
+                  <CardActionArea  sx={!postsStyleSwitcher?{display:'flex',flexDirection:'column'}:{display:'flex'}} >
+                    <CardMedia
+                      component="img"
+                      image={image}
+                      alt="Profile pic" 
+                      sx={!postsStyleSwitcher?{height:'280px',maxHeight:'300px'}:{maxWidth:'200px'}}
+                    />
+                    <FavoriteRoundedIcon color={'action'} sx={{position:'absolute', top:'5px',right:'5px',backgroundColor:'white',borderRadius:'50%',width:'35px',height:'35px'}}/>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {post.Title}                  
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {post.Description}
+                      </Typography>
+                    </CardContent>
+      
+                    <CardContent sx={{ width:'100%',display:'flex', justifyContent:'space-between'}}>
+                      <Typography gutterBottom variant="h6" component="div" sx={{fontWeight:'600'}}>
+                        {post.Price}
+                      </Typography>
+                      <BookmarkBorderOutlinedIcon fontSize="medium"/>
+                    </CardContent>
+      
+                  </CardActionArea>
+                </Card>
+              </Link>
             ))
           }
 
--- client/src/views/pages/Post.tsx
+++ client/src/views/pages/Post.tsx
@@ -6,8 +6,10 @@ import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions,Box,Paper,Grid,Divider  } from '@mui/material';
+import { Button, CardActionArea, CardActions,Box,Grid,Divider,Avatar, Stack, styled, Paper   } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
+import ForwardToInboxOutlinedIcon from '@mui/icons-material/ForwardToInboxOutlined';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 import Myimage from '../../assets/bannerBackground.png'
 import Myimages from '../../assets/ekko.jpg'
@@ -17,6 +19,16 @@ import SwipeableTextMobileStepper from '../../components/postComponents/imageSli
 import { Post } from '../../types/post';
 import { display } from '@mui/system';
 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body1,
+    padding: theme.spacing(2),
+    margin: theme.spacing(2),
+    textAlign: 'center',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    color: theme.palette.text.primary,
+  }));
 
 export default function SinglePost() {
     const images = [Myimage,Myimages,Myimage,Myimages]
@@ -47,14 +59,15 @@ export default function SinglePost() {
           {post?
             <Box sx={{display:'flex',width:'100%'}}>
                 <Card sx={{ maxWidth: '100%' }}>
+
+                    {/* images container  */}
                 <CardActionArea>
-                    <CardContent>
-                        {/* images container  */}
-                        <SwipeableTextMobileStepper data={images}/>
-                        {/* images container  */}
-                    </CardContent>
+                    <SwipeableTextMobileStepper data={images}/>
                 </CardActionArea>
+
                 <Box sx={{padding:3}}>
+
+                    {/* Post Title and Price  */}
                     <Box sx={{display:'flex',width:'100%',justifyContent:'space-between'}}>
                         <Typography gutterBottom variant="h5" component="div">
                             {post.Title}
@@ -63,17 +76,63 @@ export default function SinglePost() {
                             {post.Price} DH
                         </Typography>
                     </Box>
-                    <Divider/>
+                        <Divider/>
+
+                    {/* posts Maker Info  */}
+                    <Box sx={{display:'flex',justifyContent:'space-between', paddingTop:2,paddingBottom:2}}>
+                        <Box sx={{display:'flex',alignItems:'center'}}>
+                            <Avatar alt="Remy Sharp" src={Myimages}   sx={{ width: 80, height: 80 }}/>
+                            <Typography paddingLeft={2}>
+                                {post.Maker.FirstName} {post.Maker.LastName}
+                            </Typography>
+                        </Box>
+                        <Box sx={{display:'flex',flexDirection:'column', alignItems:'start',justifyContent:'center'}}>
+                            <Typography color={'Highlight'}>
+                                <LocalPhoneOutlinedIcon /> {post.Maker.Phone}                            
+                            </Typography>
+                            <Typography color={'Highlight'}>
+                                <ForwardToInboxOutlinedIcon/> {post.Maker.Email}                          
+                            </Typography>
+                        </Box>
+                    </Box>
+                        <Divider/>
+
+                        {/* post descrption */}
                     <Box  sx={{paddingTop:2,paddingBottom:2}}>
                         <Typography variant="h5">
                             Description:                         
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-
                             {post.Description}
                         </Typography>
                     </Box>
-                    <Divider/>
+                        <Divider/>
+
+                        {/* post Details */}
+                    <Box  sx={{paddingTop:2,paddingBottom:2}}>
+                        <Typography variant="h5">
+                            Details:                         
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <Stack>
+                                    <Item><Typography fontWeight={600}>Property type : </Typography> {post.PropertyType} </Item>
+                                    <Item><Typography fontWeight={600}>TransactionType :</Typography> {post.TransactionType} </Item>
+                                    <Item><Typography fontWeight={600}>City : </Typography>{post.City} </Item>
+                                    <Item><Typography fontWeight={600}>Sector : </Typography>{post.Sector} </Item>
+                                </Stack> 
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Stack>
+                                    {Object.entries(post.Details[0]).map(([key,value])=>(
+                                        <Item key={key}><Typography fontWeight={600}>{key} : </Typography> {value} </Item>
+                                    ))}
+                                </Stack>                           
+                            </Grid>
+                        </Grid>
+
+                    </Box>
+                        <Divider/>
                 </Box>
                 <CardActions>
                     <Button size="small" color="primary">
